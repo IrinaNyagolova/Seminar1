b7a5e4fefd0eeb761ea8acba4f028f47e48cbc23
[Ira] <[ira.nyagolova@gmail.com]>
2023-02-15 02:46:52 +0800

    FirstTask

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿int max = 0;
+
+Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int third = Convert.ToInt32(Console.ReadLine());
+
+if(first > max)
+{
+    max = first;
+}
+if(second > max)
+{
+    max = second;
+}
+if(third > max)
+{
+    max = third;
+}
+
+Console.WriteLine("max = " + max);
