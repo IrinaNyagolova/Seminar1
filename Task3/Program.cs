0c6e2b85fb220bd7bbf1ad5d9fd66ba031ff893b
[Ira] <[ira.nyagolova@gmail.com]>
2023-02-15 02:52:22 +0800

    ThirdTask

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int example = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = example % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
